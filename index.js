8b7a7be4350a2deccc5c9a7ca6dd8bde8da7dd0e
Ethan Wessel <ejwessel@gmail.com>
2020-10-07 12:10:39 -0700

    discord query for messages and reactions

--- index.js
+++ index.js
@@ -1,18 +1,74 @@
 require('dotenv').config()
+var express = require('express')
+var app = express()
+const axios = require('axios');
+const PORT = Number(process.env.PORT) || 8080
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const FWB_REACTION_ID = '754425061229854882'
+const BASE_URL = 'https://discord.com/api/'
+const CHANNEL_ID = '749443579499511860'
+const MESSAGE_LIMIT = 100 // the api max is 100
+const TOKEN = process.env.TOKEN
 
-const token = process.env.TOKEN
+async function main() {
+  const response = await axios.get(`${BASE_URL}/channels/${CHANNEL_ID}/messages`, {
+    headers: {
+      'Authorization': `Bot ${TOKEN}`,
+    },
+    params: {
+      limit: MESSAGE_LIMIT
+    }
+  })
 
-client.on('ready', () => {
-  console.log('Bot online!')
+  const { data } = response
+
+  // map FWB counts to user
+  let userCounts = {}
+  data.forEach(message => {
+    const timestamp = message.timestamp
+
+    // if time stamp is over a week from current execution then stop processing
+    const user = `${message.author.username}#${message.author.discriminator}`
+
+    if (message.reactions === undefined) {
+      return
+    }
+
+    let reactionCount = 0
+    const reactions = message.reactions
+    reactions.forEach(reaction => {
+      // skip over anything that is not FWB
+      if (reaction.emoji.id !== FWB_REACTION_ID) {
+        return
+      }
+      reactionCount += reaction.count
+    })
+
+    if (!(user in userCounts) && reactionCount > 0) {
+      userCounts[user] = reactionCount
+    } else if (user in userCounts) {
+      userCounts[user] += reactionCount
+    }
+  })
+
+  console.log(userCounts)
+}
+
+main().catch((e) => {
+  console.error(e)
+  process.exit()
 })
 
-client.on('message', msg => {
-  if (msg.content === "Hello") {
-    msg.reply("Hello Friend!")
-  }
-})
+// app.get('/', async (req, res) => {
+  // go through each message
+  // grab the timestamp, make sure timestamp is within a week
+  // grab the username 
+  // if there are reactions, grab the reactions for FWB and append to the count for a user
+  //if a user doesn't exist, then create it and add the value
 
-client.login(token)
+//   res.send(data)
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`)
+// })
